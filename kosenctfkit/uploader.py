7d8965a591b929456400261d2fa35d7034cb54d5
theoldmoon0602 <theoldmoon0602@gmail.com>
2019-07-29 20:40:15 +0900

    randomize path for challenge files

--- kosenctfkit/uploader.py
+++ kosenctfkit/uploader.py
@@ -1,10 +1,15 @@
 import os
 import shutil
 import boto3
+import os
 from base64 import b64decode
-from secrets import token_hex
 from io import BytesIO
 from PIL import Image
+from uuid import uuid4
+
+
+def unique_name():
+    return str(uuid4()).replace("-", "")
 
 
 class S3Uploader:
@@ -13,6 +18,7 @@ class S3Uploader:
             "aws_access_key_id": app.config["AWS_ACCESS_KEY"],
             "aws_secret_access_key": app.config["AWS_ACCESS_SECRET"],
         }
+        self.icon_dir = app.config["ICON_DIR"]
         self.bucket = app.config["S3_BUCKET"]
         self.region = app.config["S3_REGION"]
 
@@ -22,16 +28,15 @@ class S3Uploader:
         s3.put_object(Body=data, Bucket=self.bucket, ACL="public-read", Key=key)
 
         # get url
-        url = "{}/{}/{}".format(s3.meta.endpoint_url, self.bucket, key
-        )
+        url = "{}/{}/{}".format(s3.meta.endpoint_url, self.bucket, key)
         return url
 
     def upload_icon(self, icon_b64):
-        filename = token_hex(16)
+        filename = os.path.join(self.icon_dir, unique_name())
         return self.upload(b64decode(icon_b64), filename)
 
     def upload_attachment(self, name):
-        key = os.path.basename(name)
+        key = os.path.join(unique_name(), os.path.basename(name))
         return self.upload(open(name, "rb"), key)
 
 
@@ -42,7 +47,7 @@ class LocalUploader:
         self.static_url_path = app.static_url_path
 
     def upload_icon(self, icon_b64):
-        filename = token_hex(16) + ".png"
+        filename = unique_name() + ".png"
         filepath = os.path.join(self.icon_dir, filename)
         try:
             icon_data = b64decode(icon_b64)
@@ -57,11 +62,14 @@ class LocalUploader:
 
     def upload_attachment(self, name):
         basename = os.path.basename(name)
-        path = os.path.join(self.static_dir, basename)
+        directory = unique_name()
+        directory_path = os.path.join(self.static_dir, directory)
+        path = os.path.join(directory_path, basename)
         try:
+            os.mkdir(directory_path)
             shutil.copyfile(name, path)
         except Exception as e:
             print(e)
             return None
 
-        return os.path.join(self.static_url_path, basename)
+        return os.path.join(self.static_url_path, directory, basename)
